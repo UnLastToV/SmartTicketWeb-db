47af087f99b3a3d025c115a803b2213c62c239f8
UnLasToV <som3470100921435@gmail.com>
2024-04-07 21:16:16 +0700

    crud problem

--- app/controllers/user.controllers.js
+++ app/controllers/user.controllers.js
@@ -1,14 +1,13 @@
 const db = require('../model');
 const User = db.user;
 
-exports.Findall = (req, res) => {
+exports.findAll = (req, res) => {
     try {
-        User.Findall({
-            attributes: ["id", "name", "birthday",  "address", "licensePlate"]
+        User.findAll({
+            attributes: ["id", "name", "birthday", "address", "licensePlate"]
         })
             .then(user => {
                 res.send(user);
-                //res.status(200).json(employee); //send status nubmer too
             })
             .catch(error => {
                 console.log(error.massage);
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,9 +1,11 @@
 module.exports = (app) => {
     const user = require('../controllers/user.controllers')
 
-    const  router = require('express').Router();
+    const router = require('express').Router();
 
+    // API
     router.get("/", user.findAll);
+
 
     app.use("/users", router);
 }
